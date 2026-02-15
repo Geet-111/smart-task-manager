ccb9dec7d68dad08b198b3d0ab3bbda7fa90f60c
Geet Gulati <harmanharry960@gmail.com>
2026-02-15 10:39:10 +0530

    feat(auth): implement user registration endpoint

---
+++ SmartTaskManager.API/Controllers/AuthController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SmartTaskManager.API.Data;
+using SmartTaskManager.API.DTOs;
+using SmartTaskManager.API.Models;
+
+namespace SmartTaskManager.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AuthController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(RegisterDto dto)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var user = new User
+            {
+                Username = dto.Username,
+                PasswordHash = dto.Password, // temporarily plain text (we will hash later)
+            };
+
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return Ok(new { message = "User registered successfully" });
+        }
+    }
+}
---
+++ SmartTaskManager.API/DTOs/RegisterDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SmartTaskManager.API.DTOs
+{
+    public class RegisterDto
+    {
+        [Required]
+        [MaxLength(50)]
+        public required string Username { get; set; }
+
+        [Required]
+        [MinLength(6)]
+        public required string Password { get; set; }
+    }
+}
